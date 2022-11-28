d081a84ee141e80cc0c6f47fc65140f3cb395739
hyunjin kim <tcmhdn77@naver.com>
2022-11-28 22:23:50 +0900

    challenge_programming

---
+++ challenge/programming1/1_DeimalToHexa.c
@@ -0,0 +1,14 @@
+
+// 10진수 정수를 입력 받아서 16진수로 출력하는 프로그램을 작성해 보자.
+
+#include <stdio.h>
+
+
+int main() {
+	int num;
+	printf("decimal digit: ");
+	scanf_s("%d", &num); // 10진수 정수를 입력 받음
+	printf("hexadecimal: %x\n", num); // 16진수로 출력
+
+	return 0;
+}
